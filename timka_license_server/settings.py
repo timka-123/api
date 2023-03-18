b0e412f39d269509a243c62dc6eb51312afd6639
timka123 <git@timka.space>
2023-03-18 20:25:48 +0300

    admin login fix

--- timka_license_server/settings.py
+++ timka_license_server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['https://*.timka.space']
 
 ALLOWED_HOSTS = ['*']
 
